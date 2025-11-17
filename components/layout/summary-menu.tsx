4ea6e60a4dadf4d51f1801cfaf95c15deffbf987
Maycon Marques <mayconmarquesh@gmail.com>
2025-11-17 11:19:48 -0300

    fix: improve summary menu section mapping and layout

--- components/layout/summary-menu.tsx
+++ components/layout/summary-menu.tsx
@@ -1,4 +1,5 @@
 "use client"
+
 import { useState } from "react"
 
 import { sections } from "../sections/sections"
@@ -25,20 +26,20 @@ export const SummaryMenu = ({ onItemClick }: SummaryMenuProps) => {
           Sumário
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="bg-palette-secondary/5 border-none backdrop-blur-3xl">
+      <PopoverContent className="bg-palette-secondary/5 w-full border-none backdrop-blur-3xl">
         <h4 className="text-center">Seções</h4>
         <Separator className="bg-palette-secondary/20 my-4" />
         <div className="flex flex-col gap-2">
-          {Object.values(sections).map((section) => (
+          {Object.values(sections).map(({ htmlId, title }) => (
             <Button
-              key={section.htmlId}
+              key={htmlId}
               variant="outline"
               size="lg"
-              className="rounded-full border-transparent px-2 shadow-none"
               asChild
               onClick={onClick}
+              className="rounded-full border-transparent shadow-none"
             >
-              <a href={`#${section.htmlId}`}>{section.title}</a>
+              <a href={`#${htmlId}`}>{title}</a>
             </Button>
           ))}
         </div>
